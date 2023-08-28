cc5d4581ebc1b4644ebd58fab333662acecd66b0
DongheePaul <dlehdgml0480@naver.com>
2023-08-28 12:52:26 +0900

    Create post_write.pug. And makes router render post_write

--- routes/post.js
+++ routes/post.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//게시글 작성 페이지 렌더.
+router.get('/write', function (req, res, next) {
+    res.render('post_write');
 });
 
 module.exports = router;
