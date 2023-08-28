6d2fd655169920829282f12fb35c24f7f120040e
DongheePaul <dlehdgml0480@naver.com>
2023-08-28 13:08:59 +0900

    Make post_write.pug send data to server.

--- routes/post.js
+++ routes/post.js
@@ -7,4 +7,11 @@ router.get('/write', function (req, res, next) {
     res.render('post_write');
 });
 
+router.post('/write', function (req, res, next) {
+    const title = req.body.title;
+    const content = req.body.content;
+    console.log(title)
+    res.redirect('/');
+});
+
 module.exports = router;
